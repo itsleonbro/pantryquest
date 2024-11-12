d2a8a18ad12b3475b6de1d304b16cbc05bf96840
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-12 20:29:41 +0200

    add food card

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import defaultRecipes from "../DefaultRecipes";
+import styles from "./FoodCard.module.css";
+
+const FoodCard = ({ recipeData }) => {
+  if (!recipeData || recipeData.length === 0) {
+    return (
+      <>
+        <div>
+          <div className={styles.recipeGrid}>
+            {defaultRecipes.map(recipe => (
+              <div className={styles.recipeCard} key={recipe.id}>
+                <div>
+                  <img src={recipe.image} alt={recipe.title} />
+                </div>
+
+                <div>
+                  <h2>{recipe.title}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  console.log(recipeData);
+
+  return (
+    <div>
+      <div className={styles.recipeGrid}>
+        {recipeData.map(recipe => (
+          <div className={styles.recipeCard} key={recipe.id}>
+            <div>
+              <img src={recipe.image} alt={recipe.title} />
+            </div>
+
+            <div>
+              <h2>{recipe.title}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
