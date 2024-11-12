9e8d2606a7fe28903c47f88a0f77a5210dfebb73
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-12 20:31:31 +0200

    add home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Search from "../../components/Search/Search";
+import Categories from "../../components/Categories/Categories";
+import FoodCard from "../../components/FoodCard/FoodCard";
+
+const HomePage = ({ingredients, setIngredients, recipeData, handleSearch}) => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Search
+          ingredients={ingredients}
+          setIngredients={setIngredients}
+          handleSearch={handleSearch}
+        />
+
+        <div>
+          <Categories />
+        </div>
+
+        <div className={styles.suggestedRecipes}>
+          <h2>Suggested Recipes</h2>
+          <div className={styles.foodCard}>
+            <FoodCard recipeData={recipeData} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
