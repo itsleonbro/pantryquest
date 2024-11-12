1479383a39949feff44998203cabff509355b3b9
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-12 20:27:41 +0200

    add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>
+          <h1>PantryQuest</h1>
+        </div>
+
+        <div className={styles.rightMenu}>
+          <FavoriteBorderRoundedIcon />
+          <AccountCircleRoundedIcon />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
