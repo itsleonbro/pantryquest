92a771fccfabf029f1c9dd2e2e6280827ae911c0
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-11-13 03:42:11 +0200

    add link to logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.navContainer}>
         <div className={styles.logo}>
-          <h1>PantryQuest</h1>
+          <Link to="/home">
+            <h1>PantryQuest</h1>
+          </Link>
         </div>
 
         <div className={styles.rightMenu}>
