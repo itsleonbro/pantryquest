66395279f0fdde2f6782b3218969f45e2b2c4615
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-12-13 23:03:13 +0200

    add server file

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const UserModel = require("./models/UserModel");
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//db connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("Error connecting to MongoDB Atlas", err));
+
+//route to sign up
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = new UserModel({ username, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//route to log in
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    res.json({ message: "Logged in successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//listen
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
