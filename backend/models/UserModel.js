3b42ec90e8e4fc8242771cb0f7d0dd59e853a079
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-01-30 13:45:48 +0200

    add favourites in schema

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -2,8 +2,26 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: "string", required: true, unique: true },
+  name: { type: "string" },
+  lastName: { type: "string" },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+  favourites: [
+    {
+      recipeID: { type: String, required: true },
+      title: { type: String, required: true },
+      summary: { type: String },
+      readyInMinutes: { type: String },
+      servings: { type: String },
+      ingredients: [
+        {
+          name: { type: String },
+        },
+      ],
+      instructions: { type: String },
+    },
+  ],
 });
 
 const UserModel = mongoose.model("users", userSchema);
