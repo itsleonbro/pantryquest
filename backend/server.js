acfff40236961e01b58193da8701e67b4cde7e56
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-01-30 13:46:04 +0200

    add save and retrieve recipe endpoints

--- backend/server.js
+++ backend/server.js
@@ -103,6 +103,71 @@ app.get("/api/dashboard", authenticate, async (req, res) => {
   }
 });
 
+const randomNumber = Math.floor(Math.random() * 1000);
+
+//route to save recipe to favorites
+app.post("/api/save-recipe", authenticate, async (req, res) => {
+  const {
+    title,
+    summary,
+    readyInMinutes,
+    servings,
+    ingredients,
+    instructions,
+  } = req.body;
+  const userId = req.userId;
+
+  try {
+    const user = await UserModel.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // check if the recipe already exists in the favorites
+    if (user.favourites.some(fav => fav.title === title)) {
+      return res.status(400).json({ message: "Recipe already in favorites" });
+    }
+
+    //add recipe to favorites if not already there
+    user.favourites.push({
+      title,
+      recipeID: randomNumber,
+      summary,
+      readyInMinutes,
+      servings,
+      ingredients,
+      instructions,
+    });
+    await user.save(); // save the user with the updated favorites
+
+    res.status(200).json({ message: "Recipe added to favorites" });
+  } catch (error) {
+    console.error("Error saving recipe:", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
+//route to get user's favourite recipes
+app.get("/api/favourites", authenticate, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId, "favourites");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user.favourites); // return the favourites array
+
+    console.log("hello", user.favourites);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 //listen
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server running on port ${port}`));
